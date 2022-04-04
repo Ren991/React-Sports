b750c33e001fce24e0fe4d5ab2a473b87172b58b
Ren991 <beccrenzo@gmail.com>
2022-04-04 11:59:02 -0300

    hola

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React sports Bootcamp mindhub
         </a>
       </header>
     </div>
