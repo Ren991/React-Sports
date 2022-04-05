ac5bd9a74aa946a41e006891cdbd91c20ab92e3e
FLS08 <franciscosabato08gmail.com>
2022-04-05 18:09:47 -0300

    fix folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,6 @@ function App() {
     <>
      <BrowserRouter>
         <Header/>
-        <Home/>
         <Routes>
           <Route path="/" element={<Home />} />
           {/* <Route path="/signUp" element={ <SignUp/>}/>
