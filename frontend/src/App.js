5138094b60d3691eb749536e67f1607abca85b47
emaxisalas@gmail.com <94768370+Maxirx@users.noreply.github.com>
2022-04-05 10:39:37 -0300

    mergeComplete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,15 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import Index from './components/paginas/Index'
-import NavBar from './components/compenentes/navBar'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-
-function App() {
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-
-        <Route path='/' element={<Index />} />
-      </Routes>
-
-    </BrowserRouter>
-=======
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <>
-      <Header/>
-      <Footer/>
+      <Header />
+      <Footer />
     </>
-    
 
->>>>>>> 3d6f45752d127b5ed4e6d515ba8cdafada752713
+
   );
 }
 
