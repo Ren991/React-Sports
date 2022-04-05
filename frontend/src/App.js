0f5170393894087f8ff4fb82a2ec7d25a4f830dd
Ren991 <beccrenzo@gmail.com>
2022-04-05 10:11:44 -0300

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,22 +6,14 @@ import './styles/detalleProducto.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React sports Bootcamp mindhub commit 2 
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      {/* A modo de ejemplo! */}
+      <div className='mainHtml'>
+        <DetalleProducto/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
