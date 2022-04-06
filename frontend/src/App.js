8b48f81ce25938a9a37c83dcf93ab5ea3c4bf972
Ren991 <beccrenzo@gmail.com>
2022-04-06 12:14:32 -0300

    hola 3.5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/footer/Footer'
 import './styles/cardsView.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/pages/home';
+import DetalleProducto from './components/detalleProducto/detalleProducto'
 
 
 function App() {
