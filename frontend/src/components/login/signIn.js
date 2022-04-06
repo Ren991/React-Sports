e68ed669e7c376ca1a1282f8d5257488f59a4947
AlanTorrez <alantorrezvai@gmail.com>
2022-04-06 10:48:08 -0300

    formularios de registro y boton de google

---
+++ frontend/src/components/login/signIn.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {Form} from 'react-bootstrap'
+import { Button } from "react-bootstrap";
+import "../../styles/login.css";
+import {Link as LinkRouter} from "react-router-dom"
+import userAction from "../../redux/actions/usersActions";
+import { connect } from "react-redux";
+import GoogleSignIn from './googleSignIn'
+
+
+const SignIn = (props) => {
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userData = {
+      email: event.target[0].value,
+      password: event.target[1].value,
+      from: "signIn",
+    }
+    props.signInUser(userData)
+  }
+
+
+  return (
+    <div className="containerSignIn">
+      <h2><span className="nombreLogin">Log in MyTinerary</span></h2>
+        <p>¿Don't have an account? <LinkRouter to={"/signUp"} className="signUpButton">Create a new account!</LinkRouter></p>
+        <div className="containerGoogle">
+        <GoogleSignIn />
+        </div>
+      <Form className="formSignIn" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Remember me" />
+        </Form.Group>
+        <div className="submitContainer">
+        <Button variant="primary" type="submit" className="submitButton">
+          Log In
+        </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+const mapDispatchToProps = {
+  signInUser: userAction.signInUser,
+};
+
+export default connect(null, mapDispatchToProps)(SignIn);
