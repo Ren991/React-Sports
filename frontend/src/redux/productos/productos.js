985382f565fc23d070f7adc60a7f351c3fe818ad
emaxisalas@gmail.com <94768370+Maxirx@users.noreply.github.com>
2022-04-09 21:54:39 -0300

    restaurar

---
+++ frontend/src/redux/productos/productos.js
@@ -0,0 +1,72 @@
+import axios from "axios"
+
+const dataInicial = {
+    products: [],
+    marca: [],
+
+}
+
+export default function productosReducer(state = dataInicial, action) {
+
+    switch (action.type) {
+
+        case GET_ALL_PRODUCTS:
+            return {
+                ...state,
+                products: action.payload
+            }
+        case MARCA:
+            return {
+                ...state,
+                marca: action.payload
+            }
+        default:
+            return state
+    }
+
+}
+//ruta api      
+const URLProductos = "http://localhost:4000/api"
+//aciones
+const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS"
+const MARCA = "MARCA"
+
+export const getAllProducts = () => async (dispatch, getState) => {
+
+    const res = await axios.get(URLProductos + "/allGoods")
+    /* console.log(res); */
+    const productosTotales = res.data.respuesta.products
+    dispatch({ type: GET_ALL_PRODUCTS, payload: productosTotales })
+}
+
+export const modificarStock = (id) => {
+    const token = localStorage.getItem('token')
+    console.log(id);
+    return async (dispatch, getState) => {
+        try {
+            let response = await axios.put(URLProductos + "/allGoods/" + id, {},
+                {
+                    headers: {
+                        Authorization: "Bearer " + token
+                    }
+                })
+            console.log(response);
+            dispatch({ type: 'GET_ALL_PRODUCTS', payload: response.data.respuesta })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+export const seachProductsMarca = (id) => {
+
+    return async (dispatch, getState) => {
+
+        const res = await axios.get(URLProductos + "/allGoods/brand/" + id)
+
+        dispatch({ type: MARCA, payload: res.data.respuesta.brands })
+
+    }
+}
+
