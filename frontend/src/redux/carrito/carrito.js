15b22407f29e1106263c32118e113224821cb2e2
FLS08 <franciscosabato08gmail.com>
2022-04-14 10:57:20 -0300

    shop failed

--- frontend/src/redux/carrito/carrito.js
+++ frontend/src/redux/carrito/carrito.js
@@ -2,17 +2,10 @@
 
 
 const dataInicial = {
-<<<<<<< HEAD
-  carritoUser:[],
-  cantProducts: 0,
-  estadoCarrito:[],
-  reload:false
-=======
   carritoUser: [],
   cantProducts: 0,
   carritoParcial: []
 
->>>>>>> b47edcc953a52ff3cb55e2e70e43863a02db9496
 }
 
 export default function carritoReducer(state = dataInicial, action) {
@@ -25,27 +18,12 @@ export default function carritoReducer(state = dataInicial, action) {
         cantProducts: state.carritoUser.length + 1,
         
       }
-<<<<<<< HEAD
-    case MANTENER_ESTADO:
-      return{
-        ...state,
-        estadoCarrito: action?.payload?.split(" ")
-
-      }
-    case CAMBIATE_ESTADO:
-    
-    return{
-      ...state,
-      reload: action.payload
-    }
-=======
     case "Lista_de_ID_productos":
       return {
         ...state,
         carritoParcial: action.payload,
 
       }
->>>>>>> b47edcc953a52ff3cb55e2e70e43863a02db9496
     default:
       return state;
   }
@@ -55,35 +33,6 @@ const AGREGAR_PRODUCTO = "AGREGAR_PRODUCTO"
 const MANTENER_ESTADO = "MANTENER_ESTADO"
 const CAMBIATE_ESTADO = "CAMBIATE_ESTADO"
 export const addOneProduc = (comeProduc) => (dispatch, getState) => {
-<<<<<<< HEAD
-        
-       
-        if(localStorage.getItem("carrito") !==null){
-          const acumular = localStorage.getItem("carrito")
-          localStorage.setItem("carrito", comeProduc+ " " + acumular)
-        }else{
-          localStorage.setItem("carrito", comeProduc)
-        }
-        dispatch({ type: AGREGAR_PRODUCTO, payload: comeProduc})
-
-}
-
-export const mantenerEstado = (unArrayProduct) => (dispatch, getState) => {
-        console.log(unArrayProduct)
-         
-        dispatch({ type: MANTENER_ESTADO, payload: unArrayProduct})
-        
-
-}
-export const setReload = (unCambio) => (dispatch, getState) => {
-     
-         
-        dispatch({ type: CAMBIATE_ESTADO, payload: !unCambio})
-        
-
-}
-
-=======
   console.log(comeProduc)
   dispatch({ type: AGREGAR_PRODUCTO, payload: comeProduc })
 
@@ -95,4 +44,3 @@ export const carrryInitial = (variable) => (dispatch, getState) => {
   dispatch({ type: "Lista_de_ID_productos", payload: variable })
 
 }
->>>>>>> b47edcc953a52ff3cb55e2e70e43863a02db9496
